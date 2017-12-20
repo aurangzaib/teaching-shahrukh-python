92768fb357699c95f15e92d561ed21571a16f04b
Siddiqui <aurangzaibsiddiqui@gmail.com>
2017-12-20 06:50:26 +0100

    refactoring

--- data_structures.py
+++ data_structures.py
@@ -1,46 +1,13 @@
 """
-1 way of commenting
-"""
-
-# another way of commenting
-
-# to print something
-print("baber")
-
-# in python, we don't use any brackets for defining a function. instead, spaces are used
-# in python, we don't use ; at end of every line
-
-# in python, we dont need to define datatypes
-my_string = "baber"
-my_integer = 10
-print(my_string)
-
-
-# defining a function
-def my_function():
-    # variables are defined without types
-    a = 10
-    b = 20
-    print(a + b)
-
-
-def another_function():
-    a = 30
-    b = 420
-    c = a + b
-    return a + b
-
-
-# using a function
-# () is called invocation
-my_function()
-
-
-"""
 
 13. December 2017
 
 Data structures in Python
+    1D Array
+    2D Matrix
+    Tuple
+    Object
+    Collection
 
 """
 
